7319b274dfc52ffb703d03da74d5ad48fd39ecf1
NanXie <nan.xie@volossoftware.com>
2022-03-08 17:27:35 -0800

    Checking in code for Multiple table views in same view controller

---
+++ Assignment5_StockHistPrice/Assignment5_StockHistPrice/Constants/APIKeys.swift
@@ -0,0 +1,10 @@
+//
+//  APIKeys.swift
+//  Assignment5_StockHistPrice
+//
+//  Created by Nan  Xie on 3/2/22.
+//
+
+import Foundation
+
+let apiKey = "f6885f3ffcdb2a58447bdfc07d685ed1"
---
+++ TwoTables/TwoTables/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  TwoTables
+//
+//  Created by Nan  Xie on 3/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+
+    @IBOutlet weak var tbl1: UITableView!
+    @IBOutlet weak var tbl2: UITableView!
+    
+    var arr1 = ["Seattle", "NY", "SFO"]
+    var arr2 = ["USA", "China"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == tbl1{
+            return arr1.count
+        }else{
+            return arr2.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView == tbl1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = arr1[indexPath.row]
+            return cell
+        
+
+        
+    }else{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
+        cell.textLabel?.text = arr1[indexPath.row]
+        return cell
+    
+    }
+    
+
+
+}
+
+}
